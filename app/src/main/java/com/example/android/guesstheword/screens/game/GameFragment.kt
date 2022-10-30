b4b98a108597fef43c76def3848b168a85ff9ab2
mohamed <fuunykam1@gmail.com>
2022-10-30 19:38:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.guesstheword.screens.game
+
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.text.format.DateUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.getSystemService
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+import kotlinx.android.synthetic.main.game_fragment.*
+import kotlin.math.absoluteValue
+
+/**
+ * Fragment where the game is played
+ */
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel : GameViewModel
+
+    private lateinit var binding: GameFragmentBinding
+
+    private lateinit var correctSound: MediaPlayer
+    private lateinit var wrongSound: MediaPlayer
+    private lateinit var countDownSound: MediaPlayer
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.game_fragment,
+                container,
+                false
+        )
+
+        @Suppress("DEPRECATION")
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+
+        binding.gameViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        viewModel.eventGameFinish.observe(this.viewLifecycleOwner, Observer{ hasFinished ->
+            if (hasFinished){
+                gameFinished()
+                viewModel.onGameFinishComplete()
+            }
+        })
+
+
+        correctSound = MediaPlayer.create(context, R.raw.correct)
+        wrongSound = MediaPlayer.create(context, R.raw.wrong)
+        countDownSound = MediaPlayer.create(context, R.raw.countdown)
+        /*
+            1 for correctSound
+            2 for wrongSound
+            3 for countDownSound
+         */
+        viewModel.eventSound.observe(this.viewLifecycleOwner, Observer{ Sound ->
+            if (Sound.equals(1)) {
+                correctSound.start()
+            }
+            else if(Sound.equals(2)){
+                wrongSound.start()
+            }
+            else if (Sound.equals(3)){
+                countDownSound.start()
+            }
+        })
+
+        return binding.root
+
+    }
+
+
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
+        findNavController(this).navigate(action)
+    }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+        buzzer?.let {
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
+    }
+}
